783775ec3f48733f58c8bc21680f9fb5f56ac1f9
WeslleiBrito <wesllei.wbs@gmail.com>
2022-11-07 17:20:20 -0300

    Exercicio 1

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,15 @@
-console.log("teste 1")
+function verificaDivisivel2E3() {
+    const numero = Number(prompt('Digite um número qualquer:'))
 
+    if (numero % 2 === 0 || numero % 3 === 0) {
+        if (numero % 2 === 0) {
+            console.log(`O número ${numero} é divisível por 2.`)
+        }
+
+        if (numero % 3 === 0) {
+            console.log(`O número ${numero} é divisível por 3.`)
+        }
+    }
+}
+
+verificaDivisivel2E3()
